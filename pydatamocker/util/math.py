7b4a07f60f7f6ab6b697666916383399ecefcebc
Max Soukharev <personal.soukharev@gmail.com>
2021-07-24 14:58:53 -0400

    Fix range distribution for samples smaller than range

--- pydatamocker/util/math.py
+++ pydatamocker/util/math.py
@@ -1,8 +1,2 @@
-from math import ceil
-
-
-def range_step(min_: int, max_: int, size: int):
-    if max_ > size:
-        return ceil( (max_ - min_) / size)
-    else:
-        return (max_ - min_) / size
+def range_step(min_: int, max_: int, size: int) :
+    return (max_ - min_) / size
