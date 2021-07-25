5fa819df27dc564a85a956cd8419a6941d897502
Max Soukharev <personal.soukharev@gmail.com>
2021-07-24 22:05:20 -0400

    Refactor sampler and builder

--- pydatamocker/builder.py
+++ pydatamocker/builder.py
@@ -9,19 +9,13 @@ from .sampler import get_sample_generators
 
 def build_dataframe(fields_describe: dict, size: int) -> DataFrame:
     df = None
-    used_paths = set() # bad, bad programmer! TODO: Redesign
     for field, field_spec in fields_describe['fields'].items():
-        path = field_spec['props'].get('path')
-        if path:
-            if path in used_paths:
-                continue
-            else:
-                used_paths.add(path)
-        samples = get_sample_generators(field_spec['mock_type'], **field_spec['props'])(size)
-        for sample in samples:
-            if not type(sample) is Series:
-                name = field
-            else:
-                name = sample.name or field
-            df = concat([df, Series(sample, name=name)], axis=1)
+        print('Sampling' )
+
+        sample = get_sample_generators(field, field_spec['mock_type'], **field_spec['props'])(size)
+        if not type(sample) is Series:
+            name = field
+        else:
+            name = sample.name or field
+        df = concat([df, Series(sample, name=name)], axis=1)
     return df
--- pydatamocker/sampler.py
+++ pydatamocker/sampler.py
@@ -4,29 +4,21 @@ from .numbers import get_sample as num_sample, TYPES as NUMTYPES
 from .time import get_sample as time_sample
 
 
-def _multicolumn_sampler(dataset, size, columns):
-    sample = dataset.sample(n=size, replace=True).reset_index(drop=True)
-    return [sample[col] for col in columns]
-
-
-def _table_data_generators(path: str):
+def _table_column_sample(path: str, field_name: str):
     dataset = load_table(path)
-    columns = dataset.columns
-    return lambda size: _multicolumn_sampler(dataset, size, columns)
+    return lambda size: [dataset[field_name].sample(n=size, replace=True).reset_index(drop=True)]
 
 
-def get_sample_generators(mock_type: str, **props):
+def get_sample_generators(field_name: str, mock_type: str, **props):
     if mock_type in DATASETS:
-        return lambda size: (load_dataset(mock_type).sample(n=size, ignore_index=True, replace=True) for _ in [0])
+        return lambda size: load_dataset(mock_type).sample(n=size, ignore_index=True, replace=True)
     elif mock_type in NUMTYPES:
-        return lambda size: (num_sample(mock_type, size, **props) for _ in [0])
+        return lambda size: num_sample(mock_type, size, **props)
     elif mock_type == 'enum':
-        return lambda size: (Series(props['values']).sample(n=size, ignore_index=True, replace=True, weights=props['weights'])
-            for _ in [0])
+        return lambda size: Series(props['values']).sample(n=size, ignore_index=True, replace=True, weights=props['weights'])
     elif mock_type in { 'date', 'datetime' }:
-        return lambda size: (time_sample(mock_type, size, **props) for _ in [0])
-    elif mock_type == 'table' and props.get('path'):
-        path = props.pop('path')
-        return _table_data_generators(path)
+        return lambda size: time_sample(mock_type, size, **props)
+    elif mock_type == 'table':
+        return _table_column_sample(props['path'], field_name)
     else:
         raise ValueError('Unsupported type')
