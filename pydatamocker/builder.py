3fba38b3bd6cbb7b66b9248e7fc490171b0ca60d
Max Soukharev <maxim.soukharev@gmail.com>
2022-04-22 22:45:49 -0400

    Enable parallel building

--- pydatamocker/builder.py
+++ pydatamocker/builder.py
@@ -1,28 +1,35 @@
 from pandas import DataFrame
-
+from multiprocessing import Pool
 from pydatamocker.exceptions.builder import DATASET_AND_DATATYPE
 from .generators import dataset, datetime, enum, numeric
 from pandas import Series, DataFrame
 
+def _switch(props):
+    size = props['size']
+    del props['size']
+    datatype = props.get('datatype')
+    dataset_ = props.get('dataset')
+    if datatype and dataset_:
+        raise DATASET_AND_DATATYPE(dataset_, datatype)
+    specifier = datatype or dataset_
+    if specifier in dataset.DATASETS:
+        return dataset.generate(size, **props)
+    elif specifier in numeric.TYPES:
+        return numeric.generate(size, **props)
+    elif specifier in datetime.TYPES:
+        return datetime.generate(size, **props)
+    elif specifier == 'enum':
+        return enum.generate(size, **props)
+    else:
+        raise ValueError('Unsupported dataset or datatype ' + specifier)
+
 def build(size: int, table_spec: dict) -> DataFrame:
-    def match_generate(size, **props):
-        datatype = props.get('datatype')
-        dataset_ = props.get('dataset')
-        if datatype and dataset_:
-            raise DATASET_AND_DATATYPE(dataset_, datatype)
-        specifier = datatype or dataset_
-        if specifier in dataset.DATASETS:
-            return dataset.generate(size, **props)
-        elif specifier in numeric.TYPES:
-            return numeric.generate(size, **props)
-        elif specifier in datetime.TYPES:
-            return datetime.generate(size, **props)
-        elif specifier == 'enum':
-            return enum.generate(size, **props)
-        else:
-            raise ValueError('Unsupported dataset or datatype ' + specifier)
+
     samples = DataFrame()
-    for field_name, spec in table_spec.items():
-        sample : Series = match_generate(size, **spec)
+    with Pool(6) as p:
+        res = p.map(_switch, [
+            {**field, 'name': name, 'size': size} for name, field in table_spec.items()
+        ])
+    for field_name, sample in zip(table_spec.keys(), res):
         samples[field_name] = sample
     return samples
