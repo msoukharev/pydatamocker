ae89237e61ab2acca1bbb04dabf09ba8c0dcd7c6
Max Soukharev <maxim.soukharev@gmail.com>
2022-04-22 21:18:07 -0400

    Refactor numeric generator

--- pydatamocker/generators/numeric.py
+++ pydatamocker/generators/numeric.py
@@ -1,56 +1,56 @@
 import numpy as np
-from ..util.functions import composer
 from pandas import Series
 
-def _range_step(min_: int, max_: int, size: int) :
-    return (max_ - min_) / size
+def _range_step(min: int, max: int, size: int) :
+    return (max - min) / size
 
-samplers : dict = {
-    'float': {
-        'normal': lambda **kw: composer(
-            lambda **kw: np.random.normal(kw['mean'], kw['std'], kw['size']),
-            **kw
-        ),
-        'uniform': lambda **kw: composer(
-            lambda **kw: np.random.uniform(kw['min'], kw['max'], kw['size']),
-            **kw
-        ),
-        'range': lambda **kw: composer(
-            lambda **kw: np.arange(kw['start'], kw['end'], _range_step(kw['start'], kw['end'], kw['size'])),
-            lambda f, **kw: f.astype(float)[:kw['size']],
-            **kw
-        )
-    },
-    'integer': {
-        'uniform': lambda **kw: composer(
-            lambda **kw: np.random.randint(kw['min'], kw['max'], kw['size']),
-            **kw
-        ),
-        'binomial': lambda **kw: composer(
-            lambda **kw: np.random.binomial(kw['n'], kw['p'], kw['size']),
-            **kw
-        ),
-        'range': lambda **kw: composer(
-            lambda **kw: np.arange(kw['start'], kw['end'], _range_step(kw['start'], kw['end'], kw['size'])),
-            lambda f, **kw: f.astype(int)[:kw['size']],
-            **kw
-        )
-    }
-}
+def float_normal(**props):
+     return np.random.normal(props['mean'], props['std'], props['size'])
+
+def float_uniform(**props):
+    return np.random.uniform(props['min'], props['max'], props['size'])
+
+def float_range(**props):
+    return np.arange(props['start'], props['end'],
+        _range_step(props['start'], props['end'], props['size'])).astype(float)[:props['size']]
+
+def integer_uniform(**props):
+    return np.random.randint(props['min'], props['max'], props['size'])
+
+def integer_binomial(**props):
+    return np.random.binomial(props['n'], props['p'], props['size'])
+
+def integer_range(**props):
+    return np.arange(props['start'], props['end'],
+        _range_step(props['start'], props['end'], props['size'])).astype(int)[:props['size']]
+
+def integer_normal(mean, std, size, **_):
+    # binomial approximation to normal
+    # mean = n p
+    # std = n p (1 - p)
+    p = - (std - mean)
+    n = mean // p
+    return integer_binomial(n, p, size)
 
 TYPES = { 'float', 'integer' }
 
-DISTRIBUTIONS = {
-    'normal': { 'mean', 'std' },
-    'uniform': { 'min', 'max' },
-    'binomial' : { 'n', 'p' },
-    'range': { 'start', 'end' }
-}
-
 def generate(size: int, **props) -> Series:
+    switch_ = {
+        'float': {
+            'normal': float_normal,
+            'uniform': float_uniform,
+            'range': float_range
+        },
+        'integer': {
+            'normal': integer_normal,
+            'uniform': integer_uniform,
+            'range': integer_range,
+            'binomial': integer_binomial
+        }
+    }
     datatype = props['datatype']
     distr = props['distr']
-    nums = samplers[datatype][distr](**{ **props, 'size': size })
+    nums = switch_[datatype][distr](**{ **props, 'size': size })
     if distr == 'uniform' and props.get('round'):
         nums = np.around(nums, props['round'])
     return Series(nums)
