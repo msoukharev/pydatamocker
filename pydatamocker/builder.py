298a34e52127f75b2a90f810e94f8161f0fee70c
Max Soukharev <maxim.soukharev@gmail.com>
2022-04-23 23:14:18 -0400

    Parallel processing only for >= 100_000 size

--- pydatamocker/builder.py
+++ pydatamocker/builder.py
@@ -6,12 +6,16 @@ from .generators import factory
 def _apply(props) -> Series:
     return factory(**props)(**props)
 
-def build(size: int, table_spec: dict) -> DataFrame:
+def build(size: int, field_specs: dict) -> DataFrame:
     samples = DataFrame()
-    with Pool(6) as p:
-        res = p.map(_apply, [
-            {**field, 'name': name, 'size': size} for name, field in table_spec.items()
-        ])
-    for field_name, sample in zip(table_spec.keys(), res):
-        samples[field_name] = sample
+    if size >= 100_000:
+        with Pool(6) as p:
+            res = p.map(_apply, [
+                {**field, 'name': name, 'size': size} for name, field in field_specs.items()
+            ])
+        for field_name, sample in zip(field_specs.keys(), res):
+            samples[field_name] = sample
+    else:
+        for field_name, field_spec in field_specs.items():
+            samples[field_name] = _apply({ **field_spec, 'size': size })
     return samples
