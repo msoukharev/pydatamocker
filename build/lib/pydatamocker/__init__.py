116902cf4abf08b36d1068ea5e200ab4885f9c92
Max Soukharev <maxim.soukharev@gmail.com>
2022-06-19 22:35:53 -0400

    Ignore build

--- build/lib/pydatamocker/__init__.py
+++
@@ -1,7 +0,0 @@
-from pydatamocker.api.table import Table
-from pydatamocker.api.schema import Schema
-from pydatamocker.core import build, field
-from pydatamocker import types
-
-
-__all__ = ['Table', 'Schema', 'build', 'field', 'types']
--- build/lib/pydatamocker/util/switch.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, TypeVar
-
-V = TypeVar('V')
-R = TypeVar('R')
-
-
-def switch(test: V, switch: Dict[V, R]) -> R:
-    return switch[test]
